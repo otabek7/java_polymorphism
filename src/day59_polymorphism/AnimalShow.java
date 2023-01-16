50e75b037354a8d69cbf931aa462c526a210d4b0
Otabek M <mavlonovo@tamu.edu>
2023-01-15 19:12:27 -0600

    first commit

---
+++ src/day59_polymorphism/AnimalShow.java
@@ -0,0 +1,14 @@
+package day59_polymorphism;
+
+public class AnimalShow {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        //Polymorphism below
+        Animal zebra = new Zebra();
+        Animal cheetah = new Cheetah();
+        Animal tiger = new Tiger();
+
+        
+
+    }
+}
